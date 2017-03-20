1365f1987eaedf13ba25b684541badd4d00a999e
Philip Harries <philip.harries.unix.consulting@gmail.com>
2017-03-20 13:42:43 +0000

    search sitemap for initial sites to check

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 """ python site scraping tool """
 
+import xml.etree.ElementTree as ET
+from StringIO import StringIO
 import unicodedata
 import re
 import requests
@@ -34,6 +36,36 @@ def process_html(html_page, this_parser):
         }
 
 
+def process_xml(xml_sitemap, regex):
+    """ extract links from xml """
+    site_map_paths = set([])
+    url_paths = set([])
+    try:
+        # need to strip namespaces
+        ns_stripped = ET.iterparse(StringIO(xml_sitemap))
+        for _, element in ns_stripped:
+            if '}' in element.tag:
+                element.tag = element.tag.split('}', 1)[1]
+        xml_root = ns_stripped.root
+        for found_sitemap in xml_root.findall('sitemap'):
+            sitemap_loc = found_sitemap.find('loc')
+            new_sitemap = sitemap_loc.text
+            new_path = regex.search(new_sitemap)
+            if new_path is not None:
+                site_map_paths.add(new_path.group(1))
+        for found_url in xml_root.findall('url'):
+            url_loc = found_url.find('loc')
+            new_url = url_loc.text
+            new_path = regex.search(new_url)
+            if new_path is not None:
+                new_path = new_path.group(1)
+                url_paths.add(new_path)
+    except Exception as XML_Error:
+        print "Exception trying to parse sitemap: {}".format(XML_Error)
+        raise XML_Error
+    return (site_map_paths, url_paths)
+
+
 def main():
     """ main function """
     site = "http://wiprodigital.com"
@@ -43,6 +75,26 @@ def main():
 
     paths_to_visit = set(["index.html", "index.php"])
     paths_visited = set([])
+
+    sitemaps = set(["sitemap.xml"])
+
+    sitemaps_still_to_process = True
+    while sitemaps_still_to_process:
+        print "Processing paths..."
+        num_sitemaps = len(sitemaps)
+        for sitemap in sitemaps:
+            sitemap_url = "{}/{}".format(site, sitemap)
+            print "sitemap: {}".format(sitemap_url)
+            (xml, http_code) = http_get(sitemap_url)
+            (sitemap_paths, url_paths) = process_xml(xml, site_regex)
+            new_sitemaps = set([])
+            for sitemap_path in sitemap_paths:
+                new_sitemaps.add(sitemap_path)
+            for url_path in url_paths:
+                paths_to_visit.add(url_path)
+        sitemaps = sitemaps.union(new_sitemaps)
+        if num_sitemaps == len(sitemaps):
+            sitemaps_still_to_process = False
 
     html_parser = BuildItParser(site_regex)
 
