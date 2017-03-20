0fef4b293e28a95ef32fb5819aa351feb49e8f54
Philip Harries <philip.harries.unix.consulting@gmail.com>
2017-03-20 12:48:58 +0000

    add skeleton code for processing html page

--- scraper.py
+++ scraper.py
@@ -22,20 +22,42 @@ def http_get(url):
             return ("", 500)
 
 
+def process_html(html_page):
+    """ extract links from an html page """
+    return {
+        "int_links": [],
+        "ext_links": [],
+        "static_links": []
+        }
+
+
 def main():
     """ main function """
     site = "http://wiprodigital.com"
+    site_structure = []
 
     paths_to_visit = set(["index.html", "index.php"])
     paths_visited = set([])
 
-    for path in paths_to_visit:
-        if path not in paths_visited:
-            page_url = "{}/{}".format(site, path)
-            print "retrieving {}".format(page_url)
-            (page, code) = http_get(page_url)
-            print page  # process page for new paths
-        paths_visited.add(path)
+    paths_still_to_process = True
+    while paths_still_to_process:
+        num_paths = len(paths_to_visit)
+        new_paths = set([])
+        for path in paths_to_visit:
+            if path not in paths_visited:
+                page_url = "{}/{}".format(site, path)
+                (page, code) = http_get(page_url)
+                new_page = process_html(page)
+                new_page["path"] = path
+                site_structure.append(new_page)
+                for internal_link in new_page["int_links"]:
+                    if internal_link not in paths_visited:
+                        new_paths.add(internal_link)
+            paths_visited.add(path)
+        paths_to_visit = paths_to_visit.union(new_paths)
+        if num_paths == len(paths_to_visit):
+            # no new paths added
+            paths_still_to_process = False
 
 
 if __name__ == "__main__":
